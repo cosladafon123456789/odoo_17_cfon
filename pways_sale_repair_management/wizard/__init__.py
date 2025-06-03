f8ae7473f85031c1c97bc6334836f7e09f9b44ed
cosladafon123456789 <jc@cosladafon.com>
2025-06-03 20:07:26 +0200

    Delete pways_sale_repair_management directory

--- pways_sale_repair_management/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Preciseways See LICENSE file for full copyright and licensing details.
-from . import repair_order_wizard
-from . import replace
-from . import all_action_wizard
-from . import lot_quantity_wizard
