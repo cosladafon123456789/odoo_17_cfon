7b22581ad415da18ee560b0f1e84c40addf76edb
cosladafon123456789 <jc@cosladafon.com>
2025-07-16 21:23:38 +0200

    Add files via upload

---
+++ serial_label_test_v5/report/report_action.py
@@ -0,0 +1,8 @@
+from odoo import models
+
+class StockLot(models.Model):
+    _inherit = 'stock.lot'
+
+    def print_serial_label(self):
+        report = self.env['ir.actions.report']._get_report_from_name('serial_label_test_v5.report_serial_test_document')
+        return report.report_action(self)
