00d07758f199a8744bf2b932623ed0ca8274576e
cosladafon123456789 <jc@cosladafon.com>
2025-07-25 08:28:29 +0200

    Add files via upload

--- helpdesk_sale_order_link/models/helpdesk_ticket.py
+++ helpdesk_sale_order_link/models/helpdesk_ticket.py
@@ -1,5 +1,4 @@
 from odoo import models, fields, api
-import re
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
@@ -8,11 +7,10 @@ class HelpdeskTicket(models.Model):
 
     @api.depends('name')
     def _compute_linked_sale_order(self):
-        pattern = r'(SO\\d+)'  # Ajusta esto al formato real de tus pedidos
         for ticket in self:
             ticket.linked_sale_order_id = False
-            match = re.search(pattern, ticket.name or '')
-            if match:
-                order = self.env['sale.order'].search([('name', '=', match.group(1))], limit=1)
+            if ticket.name:
+                order_code = ticket.name.split()[0].strip()
+                order = self.env['sale.order'].search([('name', '=', order_code)], limit=1)
                 if order:
                     ticket.linked_sale_order_id = order.id
