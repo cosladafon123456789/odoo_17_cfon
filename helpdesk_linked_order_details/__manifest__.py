b2f93856624e0c2462d91586108871f0553d762c
cosladafon123456789 <jc@cosladafon.com>
2025-07-28 08:23:49 +0200

    Add files via upload

--- helpdesk_linked_order_details/__manifest__.py
+++ helpdesk_linked_order_details/__manifest__.py
@@ -1,10 +1,11 @@
 {
     "name": "Helpdesk Linked Order Details",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Helpdesk",
     "summary": "Muestra productos y nº de serie del pedido vinculado al ticket",
     "depends": ["helpdesk", "sale", "stock", "helpdesk_sale_order_link"],
     "data": [
+        "security/ir.model.access.csv",
         "views/helpdesk_ticket_view.xml"
     ],
     "installable": True,
