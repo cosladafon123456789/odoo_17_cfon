36d61a282b7b69f61dcaf3a1e53ecb939918f883
cosladafon123456789 <jc@cosladafon.com>
2025-07-28 11:07:39 +0200

    Add files via upload

--- sale_order_send_to_locations/wizards/send_to_location_wizard.py
+++ sale_order_send_to_locations/wizards/send_to_location_wizard.py
@@ -18,29 +18,45 @@ class SendToLocationWizard(models.TransientModel):
         if not picking_type:
             raise UserError("No se encontró un tipo de operación de transferencia interna para este almacén.")
 
-        moves = []
-        for line in self.sale_id.order_line:
-            if line.product_id.type != 'product':
-                continue
-            moves.append((0, 0, {
-                'name': line.name or line.product_id.name,
-                'product_id': line.product_id.id,
-                'product_uom_qty': line.product_uom_qty,
-                'product_uom': line.product_uom.id,
-                'location_id': picking_type.default_location_src_id.id,
-                'location_dest_id': self.location_id.id,
-            }))
-
-        if not moves:
-            raise UserError("No hay productos físicos válidos para transferir.")
-
         picking = self.env['stock.picking'].create({
             'picking_type_id': picking_type.id,
             'location_id': picking_type.default_location_src_id.id,
             'location_dest_id': self.location_id.id,
             'origin': self.sale_id.name,
-            'move_ids_without_package': moves,
         })
+
+        for line in self.sale_id.order_line:
+            if line.product_id.type != 'product':
+                continue
+            move = self.env['stock.move'].create({
+                'name': line.name or line.product_id.name,
+                'product_id': line.product_id.id,
+                'product_uom_qty': line.product_uom_qty,
+                'product_uom': line.product_uom.id,
+                'location_id': picking.location_id.id,
+                'location_dest_id': picking.location_dest_id.id,
+                'picking_id': picking.id,
+            })
+            if line.lot_id:
+                self.env['stock.move.line'].create({
+                    'move_id': move.id,
+                    'product_id': line.product_id.id,
+                    'qty_done': line.product_uom_qty,
+                    'product_uom_id': line.product_uom.id,
+                    'location_id': picking.location_id.id,
+                    'location_dest_id': picking.location_dest_id.id,
+                    'lot_id': line.lot_id.id,
+                    'picking_id': picking.id,
+                })
+            else:
+                move._action_confirm()
+                move._action_assign()
+                for ml in move.move_line_ids:
+                    ml.qty_done = ml.product_uom_qty
+
+        picking.action_confirm()
+        picking.action_assign()
+        picking.button_validate()
 
         return {
             'type': 'ir.actions.act_window',
