dc826f1e22a7f14d3b34d43660f1a1f65021ee9f
cosladafon123456789 <jc@cosladafon.com>
2025-07-28 10:58:44 +0200

    Add files via upload

--- sale_order_send_to_locations/__manifest__.py
+++ sale_order_send_to_locations/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Sale Order Dynamic Send to Location",
-    "version": "1.1",
+    "version": "1.2",
     "depends": ["sale", "stock"],
     "author": "ChatGPT",
     "category": "Sales",
     "description": "Adds a wizard to choose the destination inventory location from a sale order.",
     "data": [
+        "security/ir.model.access.csv",
         "views/sale_order_view.xml",
         "views/send_to_location_wizard_view.xml"
     ],
