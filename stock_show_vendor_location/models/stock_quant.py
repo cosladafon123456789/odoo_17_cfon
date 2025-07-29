96c7f0cbc3faa34040142f641e301ebc80318794
Juanma <jsantiago@nanobytes.es>
2025-07-29 13:49:36 +0200

    [DEL] eliminar módulo problemático

--- stock_show_vendor_location/models/stock_quant.py
+++
@@ -1,8 +0,0 @@
-from odoo import models
-
-class StockQuant(models.Model):
-    _inherit = 'stock.quant'
-
-    def _domain_location_id(self):
-        # Permitir mostrar también ubicaciones de proveedor
-        return [('usage', 'in', ['internal', 'supplier'])]
