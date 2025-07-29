b10d75e963fc8779d7c54551e85a1af23562d9bd
cosladafon123456789 <jc@cosladafon.com>
2025-07-29 20:46:39 +0200

    Add files via upload

---
+++ clinicphone_clientes_filtered_final/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "ClinicPhone Clientes Filtrado",
+    "version": "1.0",
+    "depends": ["stock"],
+    "author": "OpenAI",
+    "category": "Inventory",
+    "description": "Módulo que crea un menú para mostrar solo los traslados con destino Partners/Vendors/CLINICPHONE",
+    "data": [
+        "views/clinicphone_clientes_menu.xml",
+        "views/clinicphone_clientes_action.xml"
+    ],
+    "installable": True,
+    "application": True
+}
