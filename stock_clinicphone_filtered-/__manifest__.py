7a49b76d0eea8c6843de300530780e106f8d4e81
cosladafon123456789 <jc@cosladafon.com>
2025-07-29 15:08:42 +0200

    Add files via upload

---
+++ stock_clinicphone_filtered-/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Stock ClinicPhone Filtered View',
+    'version': '1.0',
+    'category': 'Inventory',
+    'summary': 'Filtro automático al inventario de ClinicPhone',
+    'author': 'ChatGPT',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_clinicphone_menu.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
