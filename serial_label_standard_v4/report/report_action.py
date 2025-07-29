313fd9a21055d033c95c65876bd746c99ec51afa
cosladafon123456789 <jc@cosladafon.com>
2025-07-29 11:04:07 +0200

    Delete serial_label_standard_v4 directory

--- serial_label_standard_v4/report/report_action.py
+++
@@ -1,8 +0,0 @@
-from odoo import models
-
-class StockLot(models.Model):
-    _inherit = 'stock.lot'
-
-    def print_serial_label(self):
-        report = self.env['ir.actions.report']._get_report_from_name('serial_label_standard_v4.report_serial_number_document')
-        return report.report_action(self)
