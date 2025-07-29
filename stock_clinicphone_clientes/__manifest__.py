031d78e3d26aa54e6032d0e7a0358a583a36f21c
cosladafon123456789 <jc@cosladafon.com>
2025-07-29 15:12:07 +0200

    Add files via upload

---
+++ stock_clinicphone_clientes/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Inventario ClinicPhone Clientes',
+    'version': '1.0',
+    'category': 'Inventory',
+    'summary': 'Ver productos en Partners/Vendors/CLINICPHONE desde un inventario personalizado',
+    'author': 'ChatGPT',
+    'depends': ['stock'],
+    'data': [
+        'views/clinicphone_clientes_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
