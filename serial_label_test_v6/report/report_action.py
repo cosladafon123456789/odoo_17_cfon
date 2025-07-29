8abf68318be5ecb9d5acbe0987968a95194e3043
cosladafon123456789 <jc@cosladafon.com>
2025-07-29 11:04:17 +0200

    Delete serial_label_test_v6 directory

--- serial_label_test_v6/report/report_action.py
+++
@@ -1,7 +0,0 @@
-from odoo import models
-
-class StockLot(models.Model):
-    _inherit = 'stock.lot'
-
-    def print_serial_label(self):
-        return self.env.ref('serial_label_test_v6.report_serial_test').report_action(self)
