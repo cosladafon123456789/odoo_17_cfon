7d19f1edeea26fcaa7d01bc9c28467851fa61ca4
cosladafon123456789 <jc@cosladafon.com>
2025-07-29 20:43:04 +0200

    Add files via upload

---
+++ clinicphone_clientes_filtered/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "ClinicPhone Clientes Filtrado",
+    "version": "1.0",
+    "category": "Inventory",
+    "depends": ["stock"],
+    "data": [
+        "views/clinicphone_clientes_action.xml",
+        "views/clinicphone_clientes_menu.xml"
+    ],
+    "installable": True,
+    "application": True
+}
