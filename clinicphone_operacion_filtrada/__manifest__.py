8bd6188648208655548c6bdb3b067ef1e657bdb2
cosladafon123456789 <jc@cosladafon.com>
2025-07-29 20:36:37 +0200

    Add files via upload

---
+++ clinicphone_operacion_filtrada/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "ClinicPhone Clientes - Operación con menú filtrado",
+    "version": "1.0",
+    "depends": ["stock"],
+    "category": "Inventory",
+    "summary": "Crea tipo de operación y acceso filtrado por ubicación ClinicPhone",
+    "data": ["data/clinicphone_operacion.xml"],
+    "installable": True,
+    "application": False
+}
