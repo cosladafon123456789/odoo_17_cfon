d18907e5d5e62869c8f120d8d11ab87e3a267a91
cosladafon123456789 <jc@cosladafon.com>
2025-08-15 10:28:26 +0200

    Add files via upload

---
+++ pways_sale_order_shipment_final/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PWays Sale Order Shipment',
+    'version': '17.0',
+    'summary': """Sale Order Shipment. RMU-14/08/2025""",
+    'description': """ Sale Order Shipment""",
+    'category': 'Sale Order',
+    'author':'Preciseways',
+    'depends': ['sale_management', 'delivery_stock_picking_batch', 'stock','sendcloud_delivery'],
+    'data': [
+        'views/inherit_stock_move_view.xml',
+        'views/sale_order_line_view.xml',
+        # 'report/report_action.xml',
+        # 'report/serial_report_template.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'price': 0.0,
+    'currency': 'EUR',
+    'images':['static/description/banner.png'],
+    'license': 'OPL-1',
+}
