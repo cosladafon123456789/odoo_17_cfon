1a281d8fd9b11eddfb973d973a0fbcab72a58af9
cosladafon123456789 <jc@cosladafon.com>
2025-08-18 12:27:06 +0200

    Add files via upload

---
+++ cfon_helpdesk_days_since_purchase_form_only/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Helpdesk: Días desde la compra (solo formulario)",
+    "summary": "Añade los campos y cálculo; muestra en formulario. Sin heredar la vista de lista.",
+    "version": "17.0.1.0.0",
+    "author": "CosladaFon + ChatGPT",
+    "license": "LGPL-3",
+    "depends": ["helpdesk", "sale"],
+    "data": [
+        "views/helpdesk_ticket_views.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
