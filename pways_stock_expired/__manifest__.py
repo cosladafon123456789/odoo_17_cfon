6c7560b9b8d079f8177bff945e6049211f500ccb
cosladafon123456789 <jc@cosladafon.com>
2025-08-20 09:08:19 +0200

    Add files via upload

---
+++ pways_stock_expired/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Pways Stock With Expired',
+    'version': '17.0',
+    'summary': """Stock With Expired. 18/08/2025""",
+    'description': """ Stock With Expired""",
+    'category': 'Stock',
+    'author':'Preciseways',
+    'depends': ['product_expiry'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'price': 0.0,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
