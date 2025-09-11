61eb91feb304560a9852c1c99fcac6605c450722
cosladafon123456789 <jc@cosladafon.com>
2025-09-11 12:54:42 +0200

    Add files via upload

---
+++ cfon_vendor_bills_bulk_upload_v3/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'CFON - Subir facturas proveedor (PDF)',
+    'version': '17.0.1.0.2',
+    'license': 'LGPL-3',
+    'author': 'ChatGPT for CFON',
+    'website': 'https://www.cosladafon.com',
+    'summary': 'Botón "Subir facturas" para arrastrar PDFs y crear borradores de facturas proveedor',
+    'category': 'Accounting/Accounting',
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/wizard_views.xml',
+        'views/account_move_views.xml',
+        
+    ],
+    'assets': {},  # no assets needed
+    'installable': True,
+    'application': False,
+}
