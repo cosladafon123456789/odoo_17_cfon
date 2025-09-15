08533fce9d24eaa4a0b7f9bf21c08334ebd13ab4
cosladafon123456789 <jc@cosladafon.com>
2025-09-15 12:58:49 +0200

    Add files via upload

---
+++ cfon_serial_return_simple/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Serial Return Simple",
+    'summary': "Very simple: adds return_count on lots, increments on customer\u2192internal returns, minimal menu.",
+    'version': "17.0.0.2.0",
+    'author': "ChatGPT for jose carlos",
+    'license': "LGPL-3",
+    'category': "Inventory/Stock",
+    'depends': ["stock"],
+    'data': ["views/stock_production_lot_views.xml"],
+    'installable': true,
+    'application': false
+}
