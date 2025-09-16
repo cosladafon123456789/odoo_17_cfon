72428311bbc2792423a7f7bc02675e87bee8a99d
cosladafon123456789 <jc@cosladafon.com>
2025-09-16 08:58:36 +0200

    Add files via upload

---
+++ pways_web_integration_final/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Website Integration',
+    'version': '1.0',
+    'category': '',
+    'summary': 'Website Integration',
+    'description': "Website Integration",
+    'depends': ['sale_management', 'sale_stock'],
+    'data': [
+        'views/sale_order_view.xml',
+        'views/res_company_view.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
