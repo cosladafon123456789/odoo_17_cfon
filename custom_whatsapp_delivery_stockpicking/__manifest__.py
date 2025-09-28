c941e1e3f2f70b19a43ffa7ebed01a5827f2f7da
cosladafon123456789 <jc@cosladafon.com>
2025-09-28 17:18:32 +0200

    Add files via upload

---
+++ custom_whatsapp_delivery_stockpicking/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'WhatsApp en Validación de Entrega (Stock Picking Context)',
+    'version': '17.0.1.0',
+    'summary': 'Envía automáticamente un WhatsApp al validar entregas, forzando siempre el modelo stock.picking',
+    'author': 'Cosladafon',
+    'depends': ['stock', 'whatsapp'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
