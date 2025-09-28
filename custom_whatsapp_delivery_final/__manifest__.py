130e6e3a59b9443d58490387627f231e1ec77c9a
cosladafon123456789 <jc@cosladafon.com>
2025-09-28 16:42:35 +0200

    Add files via upload

---
+++ custom_whatsapp_delivery_final/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'WhatsApp en Validación de Entrega (Final)',
+    'version': '17.0.1.0',
+    'summary': 'Envía automáticamente un WhatsApp al validar entregas',
+    'author': 'Cosladafon',
+    'depends': ['stock', 'whatsapp'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
