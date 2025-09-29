31f9f98db5b84c71b05d17c086446b467f3660d7
cosladafon123456789 <jc@cosladafon.com>
2025-09-29 12:33:56 +0200

    Add files via upload

---
+++ helpdesk_link_sale_order_new/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Helpdesk Link Sale Order",
+    "version": "17.0.1.0.4",
+    "depends": ["helpdesk", "sale_management"],
+    "category": "Helpdesk",
+    "summary": "Muestra tickets relacionados y botón inteligente para acceder a ellos desde el pedido de venta.",
+    "author": "CosladaFon / ChatGPT",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "data": [
+        "views/sale_order_view.xml"
+    ]
+}
