4ca55bbddda290ecd40f1253fd4fab74cad306aa
cosladafon123456789 <jc@cosladafon.com>
2025-09-29 12:39:02 +0200

    Add files via upload

---
+++ helpdesk_link_sale_order_fixed_v2/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Helpdesk Tickets in Sale Orders - FIXED v2",
+    "version": "1.0",
+    "depends": ["sale_management", "helpdesk"],
+    "data": [
+        "views/sale_order_view.xml",
+        "views/helpdesk_ticket_action.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
