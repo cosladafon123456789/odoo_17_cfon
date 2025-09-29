9e242dcd5615e60bcf691e655c03d6432449b496
cosladafon123456789 <jc@cosladafon.com>
2025-09-29 12:56:20 +0200

    Add files via upload

---
+++ cf_helpdesk_orders_fixeddd/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    "name": "CF Helpdesk Orders",
+    "version": "1.0",
+    "depends": ["sale", "helpdesk"],
+    "author": "CosladaFon",
+    "category": "Sales",
+    "data": [
+        "views/sale_order_view.xml",
+        "views/helpdesk_ticket_action.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False
+}
