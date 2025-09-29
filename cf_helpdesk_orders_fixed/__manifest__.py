513bf1af72305417f398ddae5bbbc75b84ae3c9d
cosladafon123456789 <jc@cosladafon.com>
2025-09-29 12:49:56 +0200

    Add files via upload

---
+++ cf_helpdesk_orders_fixed/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Helpdesk Tickets en Pedidos de Venta",
+    "summary": "Añade botón para ver tickets de Helpdesk relacionados con el pedido de venta",
+    "version": "1.0",
+    "category": "Sales",
+    "depends": ["sale_management", "helpdesk"],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
