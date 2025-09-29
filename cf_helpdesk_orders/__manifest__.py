1926ba650e6dc742e96284d4a2462dd0299d1dbf
cosladafon123456789 <jc@cosladafon.com>
2025-09-29 12:45:16 +0200

    Add files via upload

---
+++ cf_helpdesk_orders/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Helpdesk - Link Tickets to Sale Orders",
+    "version": "17.0.1.0.0",
+    "depends": ["sale", "helpdesk"],
+    "category": "Sales/Helpdesk",
+    "summary": "Show helpdesk tickets linked to sale orders",
+    "data": ["views/sale_order_view.xml"],
+    "installable": True,
+    "application": False,
+}
