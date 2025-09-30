7cacb9d46aba897f85759a8468406dcb933616c6
cosladafon123456789 <jc@cosladafon.com>
2025-09-30 21:07:22 +0200

    Delete pways_web_integration directory

--- pways_web_integration/models/res_company.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-
-    web_url = fields.Char(string="API URL")
-    web_token = fields.Char(string="Access Token")
