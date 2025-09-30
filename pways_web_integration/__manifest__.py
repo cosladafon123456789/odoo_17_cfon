f892bd7159dd7aed92ba1e48656c39b48644f0b0
cosladafon123456789 <jc@cosladafon.com>
2025-09-30 21:08:35 +0200

    Add files via upload

---
+++ pways_web_integration/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Website Integration',
+    'version': '1.0',
+    'category': '',
+    'summary': 'Website Integration 30/09/2025',
+    'description': "Website Integration 30/09/2025",
+    'depends': ['sale_management', 'sale_stock'],
+    'data': [
+        'views/sale_order_view.xml',
+        'views/res_company_view.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ pways_web_integration/models/sale_order.py
@@ -0,0 +1,58 @@
+from odoo import api, fields, models
+import requests 
+
+class SaleOrder(models.Model):
+	_inherit = "sale.order"
+
+	sync_status = fields.Selection([('failure_9', 'Fail Cancel Order'), ('failure_10', 'Fail Delivery'), ('done', 'Done')], string = "Sync Status", readonly=True, copy=False)
+
+	def action_cancel(self):
+		res = super(SaleOrder, self).action_cancel()
+		for sale in self:
+			sale._notify_delivery_status()
+		return res
+
+	def _notify_delivery_status(self, status=9):
+		# loginc for send request
+		url = self.env.company.web_url
+		if url and self.env.company.web_token:
+			headers = {"Content-Type": "application/json", "X-Webhook-Token": self.env.company.web_token}
+			failure_status = "failure_"+ str(status)
+			payload = {
+				"orders": [{
+					"order_number": self.name,
+					"status": status
+				}]
+			}
+			try:
+				answer = requests.post(url, data=payload, headers=headers)
+				if answer.status_code == 200:
+					self.sync_status = "done"
+				else:
+					self.sync_status = failure_status
+			except requests.exceptions.ConnectionError:
+				self.sync_status = failure_status
+		return True
+
+	@api.model
+	def call_webhook_integration(self):
+		failed_orders = self.env['sale.order'].search([('sync_status', 'in', ["failure_9", "failure_10"])])
+		for sale in failed_orders:
+			if sale.sync_status == "failure_9":
+				sale._notify_delivery_status()
+			else:
+				sale._notify_delivery_status(status=10)
+		return True
+
+
+class StockPicking(models.Model):
+	_inherit = "stock.picking"
+
+	def button_validate(self):
+		res = super(StockPicking, self).button_validate()
+		for picking in self:
+			if picking.sale_id and picking.sale_id.delivery_status != "pending":
+				picking.sale_id._notify_delivery_status(status=10)
+		return res
+
+	
