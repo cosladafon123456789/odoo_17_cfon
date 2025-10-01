fc21309bf038a7c4faba1b391cb92faba9d80451
cosladafon123456789 <jc@cosladafon.com>
2025-10-01 10:20:49 +0200

    Add files via upload

---
+++ cf_helpdesk_motivos_v/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'CF Helpdesk Motivos',
+    'version': '17.0.2.0',
+    'summary': 'Gestión de motivos de devolución en Helpdesk',
+    'description': 'Añade un campo motivo de devolución, obligatorio en ciertas etapas y disponible en informes.',
+    'author': 'CosladaFon',
+    'depends': ['helpdesk'],
+    'data': [
+        'views/helpdesk_ticket_views.xml',
+        'views/helpdesk_ticket_reports.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
