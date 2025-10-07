4fd487a805d28517fd41edf11559e67c9aab6fc5
cosladafon123456789 <jc@cosladafon.com>
2025-10-08 01:45:08 +0200

    Update __manifest__.py

--- marketplace_helpdesk/__manifest__.py
+++ marketplace_helpdesk/__manifest__.py
@@ -1,18 +1,17 @@
 {
-    "name": "Marketplaces Helpdesk",
-    "summary": "Tickets de marketplaces (base) con vistas y menús",
-    "description": "Módulo base para gestionar tickets de marketplaces. Incluye modelos, vistas, menús y filtros ‘No leídos’/‘Todos’. Sin cron ni ajustes.",
-    "author": "CFON",
+    "name": "Marketplace Helpdesk",
+    "version": "1.0",
     "category": "Helpdesk",
-    "version": "17.0.1.0.0",
-    "license": "LGPL-3",
+    "summary": "Gestión de tickets de marketplaces como Mirakl, MediaMarkt, etc.",
+    "author": "CFON Telecomunicaciones",
+    "depends": ["base", "helpdesk"],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/marketplace_account_views.xml",
+        "views/marketplace_ticket_views.xml",   # 👈 esta va antes
+        "views/menu.xml",                       # 👈 esta va después
+    ],
     "application": True,
-    "depends": ["base", "mail"],
-   "data": [
-    "security/security.xml",
-    "security/ir.model.access.csv",
-    "views/menu.xml",
-    "views/marketplace_account_views.xml",
-    "views/marketplace_ticket_views.xml",
-],
+    "installable": True,
 }
