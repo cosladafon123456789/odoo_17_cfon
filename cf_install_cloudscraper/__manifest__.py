08e95ec87e757de431729f4d1b4a06630595177c
cosladafon123456789 <jc@cosladafon.com>
2025-10-07 23:31:25 +0200

    Add files via upload

---
+++ cf_install_cloudscraper/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'CF Install CloudsScraper',
+    'version': '17.0.1.0.0',
+    'summary': 'Instala automáticamente el paquete Python cloudscraper al instalar el módulo.',
+    'category': 'Tools',
+    'author': 'CosladaFon',
+    'website': 'https://cosladafon.com',
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+    'post_init_hook': 'post_init_hook',
+}
---
+++ cf_install_cloudscraper/models/installer.py
@@ -0,0 +1,22 @@
+import subprocess
+import sys
+import logging
+
+from odoo import api
+
+_logger = logging.getLogger(__name__)
+
+def _install_cloudscraper(cr, registry):
+    try:
+        import cloudscraper
+        _logger.info("✅ El paquete 'cloudscraper' ya está instalado.")
+    except ImportError:
+        _logger.warning("⚠️ El paquete 'cloudscraper' no está instalado. Intentando instalarlo...")
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "cloudscraper"])
+            _logger.info("✅ 'cloudscraper' instalado correctamente.")
+        except Exception as e:
+            _logger.error(f"❌ Error al instalar cloudscraper: {e}")
+
+def post_init_hook(cr, registry):
+    _install_cloudscraper(cr, registry)
