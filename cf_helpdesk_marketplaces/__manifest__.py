8ad9d1d97605f121dfc343f0dcaed625a10934cf
cosladafon123456789 <jc@cosladafon.com>
2025-10-08 17:32:15 +0200

    Update __manifest__.py

--- cf_helpdesk_marketplaces/__manifest__.py
+++ cf_helpdesk_marketplaces/__manifest__.py
@@ -1,12 +1,20 @@
-{"name": "Marketplace Helpdesk"
-"version": "17.0.1.0.0"
-"category": "Helpdesk"
-"summary": "Centraliza mensajes de marketplaces (Mirakl, etc.) en un helpdesk propio y permite responder."
-"author": "CFON Telecomunicaciones"
-"website": "https://cosladafon.com"
-"depends": ["base", "mail", "contacts"]
-"data": ["security/security.xml", "security/ir.model.access.csv", "views/marketplace_account_views.xml", "views/marketplace_ticket_views.xml", "views/menu.xml", "data/ir_cron.xml"]
-"assets": {}
-"application": true
-"installable": true
-"license": "LGPL-3"}
+{
+    "name": "Marketplace Helpdesk",
+    "version": "17.0.1.0.0",
+    "category": "Helpdesk",
+    "summary": "Centraliza mensajes de marketplaces (Mirakl, etc.) en un helpdesk propio y permite responder.",
+    "author": "CFON Telecomunicaciones",
+    "website": "https://cosladafon.com",
+    "depends": ["base", "mail", "contacts"],
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/marketplace_account_views.xml",
+        "views/marketplace_ticket_views.xml",
+        "views/menu.xml",
+        "data/ir_cron.xml",
+    ],
+    "application": True,
+    "installable": True,
+    "license": "LGPL-3",
+}
