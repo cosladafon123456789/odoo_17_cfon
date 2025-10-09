c9b18bbc3b9c8b820e429a2921fec7762f6c902b
cosladafon123456789 <jc@cosladafon.com>
2025-10-09 11:00:23 +0200

    Update sale_order.py

--- pways_web_integration/models/sale_order.py
+++ pways_web_integration/models/sale_order.py
@@ -5,7 +5,12 @@ import json
 class SaleOrder(models.Model):
 	_inherit = "sale.order"
 
-	sync_status = fields.Selection([('failure_9', 'Fail Cancel Order'), ('failure_10', 'Fail Delivery'), ('done', 'Done')], string = "Sync Status", readonly=True, copy=False)
+	sync_status = fields.Selection([
+		('failure_9', 'Fail Cancel Order'),
+		('failure_10', 'Fail Delivery'),
+		('failure_11', 'Fail Refund'),
+		('done', 'Done')
+	], string="Sync Status", readonly=True, copy=False)
 
 	def action_cancel(self):
 		res = super(SaleOrder, self).action_cancel()
@@ -13,12 +18,21 @@ class SaleOrder(models.Model):
 			sale._notify_delivery_status()
 		return res
 
+	def button_refund(self):
+		res = super(SaleOrder, self).button_refund()
+		for sale in self:
+			sale._notify_delivery_status(status=11)
+		return res
+
 	def _notify_delivery_status(self, status=9):
-		# loginc for send request
+		# lógica para enviar request
 		url = self.env.company.web_url
 		if url and self.env.company.web_token:
-			headers = {"Content-Type": "application/json", "X-Webhook-Token": self.env.company.web_token}
-			failure_status = "failure_"+ str(status)
+			headers = {
+				"Content-Type": "application/json",
+				"X-Webhook-Token": self.env.company.web_token
+			}
+			failure_status = "failure_" + str(status)
 			payload = {
 				"orders": [{
 					"order_number": self.name,
@@ -29,7 +43,7 @@ class SaleOrder(models.Model):
 			try:
 				payload = json.dumps(payload)
 				answer = requests.post(url, data=payload, headers=headers)
-				print("answer.........",answer)
+				print("answer.........", answer)
 				if answer.status_code == 200:
 					self.sync_status = "done"
 				else:
@@ -40,12 +54,16 @@ class SaleOrder(models.Model):
 
 	@api.model
 	def call_webhook_integration(self):
-		failed_orders = self.env['sale.order'].search([('sync_status', 'in', ["failure_9", "failure_10"])])
+		failed_orders = self.env['sale.order'].search([
+			('sync_status', 'in', ["failure_9", "failure_10", "failure_11"])
+		])
 		for sale in failed_orders:
 			if sale.sync_status == "failure_9":
 				sale._notify_delivery_status()
-			else:
+			elif sale.sync_status == "failure_10":
 				sale._notify_delivery_status(status=10)
+			elif sale.sync_status == "failure_11":
+				sale._notify_delivery_status(status=11)
 		return True
 
 
@@ -58,5 +76,3 @@ class StockPicking(models.Model):
 			if picking.sale_id and picking.sale_id.delivery_status != "pending":
 				picking.sale_id._notify_delivery_status(status=10)
 		return res
-
-	
