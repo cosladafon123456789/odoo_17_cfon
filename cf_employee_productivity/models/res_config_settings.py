a1e2686b895fb06c2e56d2efeb10bef17e0e6954
cosladafon123456789 <jc@cosladafon.com>
2025-10-11 08:22:39 +0200

    Add files via upload

--- cf_employee_productivity/models/res_config_settings.py
+++ cf_employee_productivity/models/res_config_settings.py
@@ -1,9 +1,4 @@
 from odoo import api, fields, models
-import json
-
-PARAM_PICKING_USERS = "cf_employee_productivity.picking_user_ids"
-PARAM_REPAIR_USERS = "cf_employee_productivity.repair_user_ids"
-PARAM_HELPDESK_USERS = "cf_employee_productivity.helpdesk_user_ids"
 
 def _ids_to_param(ids):
     return ",".join(str(int(i)) for i in ids) if ids else ""
@@ -16,15 +11,36 @@ def _param_to_ids(param):
     except Exception:
         return []
 
+PARAM_PICKING_USERS = "cf_employee_productivity.picking_user_ids"
+PARAM_REPAIR_USERS = "cf_employee_productivity.repair_user_ids"
+PARAM_HELPDESK_USERS = "cf_employee_productivity.helpdesk_user_ids"
+PARAM_SEND_DAILY = "cf_employee_productivity.send_daily_summary"
+PARAM_RECIPIENT_IDS = "cf_employee_productivity.summary_recipient_ids"
+
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    picking_user_ids = fields.Many2many("res.users", string="Usuarios de pedidos a contabilizar",
-                                        help="Usuarios cuyos pickings validados se contabilizan")
-    repair_user_ids = fields.Many2many("res.users", string="Usuarios técnicos a contabilizar",
-                                       help="Usuarios cuyas reparaciones finalizadas se contabilizan")
-    helpdesk_user_ids = fields.Many2many("res.users", string="Usuarios de postventa a contabilizar",
-                                         help="Usuarios cuyas respuestas en tickets se contabilizan")
+    picking_user_ids = fields.Many2many(
+        "res.users", 
+        "rel_cf_productivity_picking_users", 
+        string="Usuarios de pedidos a contabilizar"
+    )
+    repair_user_ids = fields.Many2many(
+        "res.users", 
+        "rel_cf_productivity_repair_users", 
+        string="Usuarios técnicos a contabilizar"
+    )
+    helpdesk_user_ids = fields.Many2many(
+        "res.users", 
+        "rel_cf_productivity_helpdesk_users", 
+        string="Usuarios de postventa a contabilizar"
+    )
+    send_daily_summary = fields.Boolean("Enviar resumen diario por email", default=True)
+    summary_recipient_ids = fields.Many2many(
+        "res.users",
+        "rel_cf_productivity_summary_recipients",
+        string="Destinatarios del resumen diario"
+    )
 
     @api.model
     def get_values(self):
@@ -34,6 +50,8 @@ class ResConfigSettings(models.TransientModel):
             picking_user_ids=[(6, 0, _param_to_ids(icp.get_param(PARAM_PICKING_USERS)))],
             repair_user_ids=[(6, 0, _param_to_ids(icp.get_param(PARAM_REPAIR_USERS)))],
             helpdesk_user_ids=[(6, 0, _param_to_ids(icp.get_param(PARAM_HELPDESK_USERS)))],
+            send_daily_summary=True if icp.get_param(PARAM_SEND_DAILY, "1") in ("1","True","true") else False,
+            summary_recipient_ids=[(6, 0, _param_to_ids(icp.get_param(PARAM_RECIPIENT_IDS)))],
         )
         return res
 
@@ -43,8 +61,9 @@ class ResConfigSettings(models.TransientModel):
         icp.set_param(PARAM_PICKING_USERS, _ids_to_param(self.picking_user_ids.ids))
         icp.set_param(PARAM_REPAIR_USERS, _ids_to_param(self.repair_user_ids.ids))
         icp.set_param(PARAM_HELPDESK_USERS, _ids_to_param(self.helpdesk_user_ids.ids))
+        icp.set_param(PARAM_SEND_DAILY, "1" if self.send_daily_summary else "0")
+        icp.set_param(PARAM_RECIPIENT_IDS, _ids_to_param(self.summary_recipient_ids.ids))
 
-    # Helpers for other models
     @api.model
     def _get_configured_user_ids(self, param_key):
         icp = self.env["ir.config_parameter"].sudo()
