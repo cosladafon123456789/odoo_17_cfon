83143b50b70189219f5c04cb523e7b0595ffcb8c
cosladafon123456789 <jc@cosladafon.com>
2025-10-11 10:27:24 +0200

    Update __manifest__.py

--- cf_productivity/__manifest__.py
+++ cf_productivity/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "CF Productivity",
-    "version": "17.0.1.0.1",
+    "version": "17.0.1.1.0",
     "category": "Productivity",
     "summary": "Dashboard, métricas y configuración de productividad (sin cron)",
     "author": "CFON Telecomunicaciones",
@@ -8,11 +8,8 @@
     "depends": ["base", "stock", "repair", "helpdesk", "mail"],
     "data": [
         "security/ir.model.access.csv",
-        "wizard/repair_reason_wizard_views.xml",
-        "data/placeholder.xml",
-        "views/productivity_menu.xml",
         "views/res_config_settings_views.xml",
-        "views/productivity_views.xml"
+        "views/productivity_menu.xml"
     ],
     "images": ["static/description/icon.png"],
     "application": True,
