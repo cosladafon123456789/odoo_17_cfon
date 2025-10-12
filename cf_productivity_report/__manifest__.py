cd30d1e1f95d3d3cdb235bf196641993be1df3ae
cosladafon123456789 <jc@cosladafon.com>
2025-10-12 20:32:33 +0200

    Add files via upload

--- cf_productivity_report/__manifest__.py
+++ cf_productivity_report/__manifest__.py
@@ -1,18 +1,17 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Productividad CF",
-    "summary": "Contabiliza productividad: reparaciones finalizadas (con motivos), tickets respondidos y entregas validadas. Dashboard estilo Devoluciones.",
-    "version": "17.0.1.0.1",
-    "author": "CFON Telecomunicaciones",
-    "website": "https://cosladafon.com",
-    "license": "LGPL-3",
-    "depends": ["base", "base_setup", "repair", "helpdesk", "stock"],
+    "version": "1.0",
+    "category": "Productividad",
+    "summary": "Control de productividad (Reparaciones, Tickets, Ventas)",
+    "author": "CosladaFon",
+    "depends": ["base", "repair", "helpdesk", "stock", "base_setup"],
     "data": [
         "security/security.xml",
-        "views/repair_reason_wizard_views.xml",
         "views/productivity_views.xml",
         "views/productivity_menu.xml",
         "views/company_productivity_views.xml",
+        "views/res_config_settings_views.xml",
+        "views/repair_reason_wizard_views.xml",
         "security/ir.model.access.csv"
     ],
     "application": True,
