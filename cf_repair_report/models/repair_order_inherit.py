0bd317f7abe817763d465998066da27cdfc789ed
cosladafon123456789 <jc@cosladafon.com>
2025-10-12 18:30:30 +0200

    Add files via upload

--- cf_repair_report/models/repair_order_inherit.py
+++ cf_repair_report/models/repair_order_inherit.py
@@ -1,14 +1,10 @@
-
 from odoo import models
-from odoo.exceptions import UserError
 
 class RepairOrder(models.Model):
     _inherit = "repair.order"
 
     def open_reason_wizard(self):
         self.ensure_one()
-        if self.state not in ("under_repair", "confirmed"):
-            raise UserError("Solo puedes finalizar reparaciones en estado 'Confirmado' o 'En reparación'.")
         return {
             "type": "ir.actions.act_window",
             "res_model": "repair.reason.wizard",
