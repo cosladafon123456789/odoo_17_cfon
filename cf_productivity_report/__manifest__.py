9a06ab9b975be4d8dce638ee697bf00c559ad7ed
cosladafon123456789 <jc@cosladafon.com>
2025-10-12 20:34:29 +0200

    Add files via upload

--- cf_productivity_report/__manifest__.py
+++ cf_productivity_report/__manifest__.py
@@ -7,13 +7,13 @@
     "depends": ["base", "repair", "helpdesk", "stock", "base_setup"],
     "data": [
         "security/security.xml",
+        "security/ir.model.access.csv",
         "views/productivity_views.xml",
         "views/productivity_menu.xml",
         "views/company_productivity_views.xml",
         "views/res_config_settings_views.xml",
-        "views/repair_reason_wizard_views.xml",
-        "security/ir.model.access.csv"
+        "views/repair_reason_wizard_views.xml"
     ],
     "application": True,
-    "installable": True,
+    "installable": True
 }
