a2040b63c7d4c30a845d2c468700d2b429e87b53
cosladafon123456789 <jc@cosladafon.com>
2025-10-12 20:28:21 +0200

    Add files via upload

--- cf_productivity_report/__manifest__.py
+++ cf_productivity_report/__manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "CF Productividad - Reparaciones, Tickets y Pedidos",
+    "name": "Productividad CF",
     "summary": "Contabiliza productividad: reparaciones finalizadas (con motivos), tickets respondidos y entregas validadas. Dashboard estilo Devoluciones.",
     "version": "17.0.1.0.1",
     "author": "CFON Telecomunicaciones",
     "website": "https://cosladafon.com",
     "license": "LGPL-3",
-    "depends": ["base", "mail", "sale_management", "stock", "helpdesk", "repair", "base_setup"],
+    "depends": ["base", "base_setup", "repair", "helpdesk", "stock"],
     "data": [
         "security/security.xml",
-        "views/productivity_views.xml",
         "views/repair_reason_wizard_views.xml",
+        "views/productivity_views.xml",
         "views/productivity_menu.xml",
         "views/company_productivity_views.xml",
         "security/ir.model.access.csv"
--- cf_productivity_report/models/__init__.py
+++ cf_productivity_report/models/__init__.py
@@ -1,6 +1,6 @@
-from . import res_config_settings
 from . import productivity_line
 from . import repair_inherit
 from . import helpdesk_inherit
 from . import stock_picking_inherit
+from . import res_config_settings
 from . import res_company_inherit
