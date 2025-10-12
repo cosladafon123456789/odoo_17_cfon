f19985cb0f0f71677d801290b094586bf6cd8ac6
cosladafon123456789 <jc@cosladafon.com>
2025-10-12 18:40:52 +0200

    Add files via upload

--- cf_repair_report_clean/__manifest__.py
+++ cf_repair_report_clean/__manifest__.py
@@ -1,16 +1,17 @@
+
 {
     "name": "Informe de Reparaciones",
-    "version": "17.0.1.0.1",
+    "version": "17.0.1.0.0",
     "summary": "Wizard al finalizar reparación para registrar motivo y generar informe diario por técnico",
     "author": "CFON Telecomunicaciones",
     "license": "LGPL-3",
     "depends": ["repair"],
     "data": [
+        "security/ir.model.access.csv",
         "views/repair_order_inherit_views.xml",
         "views/repair_reason_wizard_views.xml",
         "views/repair_report_views.xml",
-        "views/menus.xml",
-        "security/ir.model.access.csv"
+        "views/menus.xml"
     ],
     "installable": True,
     "application": False
