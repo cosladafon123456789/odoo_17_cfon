fb317503a2ba89bff24538e8617f443d6d5942af
cosladafon123456789 <jc@cosladafon.com>
2025-10-13 20:11:51 +0200

    Delete cf_productivity_report directory

--- cf_productivity_report/models/res_company_inherit.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-
-    cf_user_repair_id = fields.Many2one("res.users", string="Usuario de reparaciones")
-    cf_user_ticket_id = fields.Many2one("res.users", string="Usuario de tickets")
-    cf_user_order_id  = fields.Many2one("res.users", string="Usuario de pedidos/entregas")
