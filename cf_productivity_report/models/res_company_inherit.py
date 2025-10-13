2f814e5e5b9a8af555c68c50152287c95a075418
cosladafon123456789 <jc@cosladafon.com>
2025-10-13 16:39:12 +0200

    Add files via upload

--- cf_productivity_report/models/res_company_inherit.py
+++ cf_productivity_report/models/res_company_inherit.py
@@ -8,10 +8,9 @@ class ResCompany(models.Model):
     cf_user_ticket_id = fields.Many2one("res.users", string="Usuario de tickets")
     cf_user_order_id  = fields.Many2one("res.users", string="Usuario de pedidos/entregas")
 
-# Timeout de reseteo en minutos para intervalos entre validaciones
-cf_order_timeout_min = fields.Integer(
-    string="Timeout validaciones (min)",
-    default=30,
-    help="Si entre validaciones pasa más de este tiempo, se considera nueva sesión y no se promedia."
-)
-
+    # Timeout de reseteo en minutos para intervalos entre validaciones
+    cf_order_timeout_min = fields.Integer(
+        string="Timeout validaciones (min)",
+        default=30,
+        help="Si entre validaciones pasa más de este tiempo, se considera nueva sesión y no se promedia."
+    )
