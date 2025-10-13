c575426ef62a3d1b05a9a4428fb99808aefaac22
cosladafon123456789 <jc@cosladafon.com>
2025-10-13 16:17:25 +0200

    Delete cf_productivity_report_pro_v3 directory

--- cf_productivity_report_pro_v3/models/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from . import productivity_line
-from . import repair_reason_wizard
-from . import repair_inherit
-from . import helpdesk_inherit
-from . import stock_picking_inherit
-from . import res_company_inherit
-from . import productivity_goal
