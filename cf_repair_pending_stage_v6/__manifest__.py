fd57d5e0abe26eb6b3ee21a9e26e49f4695b5e65
cosladafon123456789 <jc@cosladafon.com>
2025-10-15 11:22:44 +0200

    Add files via upload

---
+++ cf_repair_pending_stage_v6/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'CF Repair Pending Stage',
+    'version': '17.0.1.5',
+    'summary': 'Añade estado, botón y filtro de búsqueda "Pendiente de pieza" en reparaciones.',
+    'depends': ['repair'],
+    'data': ['views/repair_order_view.xml'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False
+}
