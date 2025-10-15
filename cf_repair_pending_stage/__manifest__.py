7146ed420a03fad9050d10e49329f0a1c3b865b2
cosladafon123456789 <jc@cosladafon.com>
2025-10-15 10:26:28 +0200

    Add files via upload

---
+++ cf_repair_pending_stage/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'CF Repair Pending Stage',
+    'version': '17.0.1.0',
+    'depends': ['mrp_repair'],
+    'data': [
+        'views/repair_order_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
