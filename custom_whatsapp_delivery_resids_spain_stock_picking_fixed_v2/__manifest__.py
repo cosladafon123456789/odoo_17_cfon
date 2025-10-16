2831bee1abb3c72195c4d45838c84b1a4fcf1ec6
cosladafon123456789 <jc@cosladafon.com>
2025-10-16 19:20:29 +0200

    Add files via upload

---
+++ custom_whatsapp_delivery_resids_spain_stock_picking_fixed_v2/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Custom WhatsApp Delivery Resid Spain (Fixed v2)',
+    'version': '17.0.1.2',
+    'author': 'CosladaFon',
+    'depends': ['stock', 'whatsapp'],
+    'data': [],
+    'installable': True,
+}
