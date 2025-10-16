71972c29a7f4cb2f05386af69d52ca0177f9736f
cosladafon123456789 <jc@cosladafon.com>
2025-10-16 19:18:44 +0200

    Delete custom_whatsapp_delivery_resids_spain_stock_picking_fixed directory

--- custom_whatsapp_delivery_resids_spain_stock_picking_fixed/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    'name': 'Custom WhatsApp Delivery Resid Spain (Fixed)',
-    'version': '17.0.1.1',
-    'author': 'CosladaFon',
-    'depends': ['stock', 'whatsapp'],
-    'data': [],
-    'installable': True,
-}
