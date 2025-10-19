e753c23f8887f1d6c000daa926e1addd843423b9
cosladafon123456789 <jc@cosladafon.com>
2025-10-19 18:27:45 +0200

    Add files via upload

---
+++ cf_productivity_report_pro_visual_final/__manifest__.py
@@ -0,0 +1,22 @@
+
+{
+    'name': 'CF Productividad Pro',
+    'summary': 'Suite avanzada de productividad: intervalos, ranking, pausas, dashboard y KPIs',
+    'version': '17.0.1.0.0',
+    'author': 'CFON / ChatGPT',
+    'category': 'Productivity',
+    'depends': ['base', 'mail'],
+    'data': [
+        'security/cf_productivity_security.xml',
+        'security/ir.model.access.csv',
+        'data/ir_cron.xml',
+        'views/productivity_views.xml',
+        'views/productivity_stats_views.xml',
+        'views/productivity_ranking_views.xml',
+        'views/dashboard_views.xml',
+        'views/mail_template.xml'
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+}
