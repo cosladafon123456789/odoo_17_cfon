3e481e7ae1a15a03303cc07e93c88b95b0d37ab8
cosladafon123456789 <jc@cosladafon.com>
2025-10-20 16:38:36 +0200

    Add files via upload

--- cf_productivity_report_pro_final/__manifest__.py
+++ cf_productivity_report_pro_final/__manifest__.py
@@ -5,16 +5,16 @@
     'author': 'CFON / ChatGPT',
     'category': 'Productivity',
     'depends': ['base', 'mail'],
-    'data': [
+        'data': [
         'security/cf_productivity_security.xml',
         'security/ir.model.access.csv',
-        'data/ir_cron.xml',
-        'data/server_actions.xml',
         'views/productivity_views.xml',
         'views/productivity_stats_views.xml',
         'views/productivity_ranking_views.xml',
         'views/dashboard_views.xml',
-        'views/mail_template.xml'
+        'views/mail_template.xml',
+        'data/ir_cron.xml',
+        'data/server_actions.xml',
     ],
     'license': 'LGPL-3',
     'installable': True,
