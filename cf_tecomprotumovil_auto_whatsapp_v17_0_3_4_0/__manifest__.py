1b38268bf7a534111b62e94d66d2dd94a35c9f0a
cosladafon123456789 <jc@cosladafon.com>
2025-10-21 09:24:26 +0200

    Add files via upload

---
+++ cf_tecomprotumovil_auto_whatsapp_v17_0_3_4_0/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "TeComproTuMovil – Envío automático WhatsApp (Definitivo)",
+    "summary": "Envía la plantilla 'tecomprotumovil' al validar recepciones. Evita duplicados por mobile_number. Compatible con WhatsApp oficial Odoo 17.",
+    "version": "17.0.3.4.0",
+    "author": "CosladaFon",
+    "website": "https://www.cosladafon.com",
+    "category": "Purchase/WhatsApp",
+    "license": "OEEL-1",
+    "depends": ["purchase_stock", "stock", "whatsapp"],
+    "data": [],
+    "application": False,
+    "installable": True
+}
