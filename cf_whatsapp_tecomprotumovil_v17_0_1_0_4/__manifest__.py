6b47111e4422f491bf624059ca6c78221e8d63c1
cosladafon123456789 <jc@cosladafon.com>
2025-10-21 11:07:02 +0200

    Add files via upload

---
+++ cf_whatsapp_tecomprotumovil_v17_0_1_0_4/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'CF WhatsApp TeComproTuMovil',
+    'version': '17.0.1.0.4',
+    'author': 'CFON Telecomunicaciones',
+    'category': 'Stock',
+    'summary': 'Envía automáticamente mensaje de WhatsApp TeComproTuMovil al validar una recepción',
+    'depends': ['stock', 'purchase', 'whatsapp'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
---
+++ cf_whatsapp_tecomprotumovil_v17_0_1_0_4/models/stock_picking.py
@@ -0,0 +1,47 @@
+from odoo import models, api
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.model
+    def button_validate(self, *args, **kwargs):
+        res = super().button_validate(*args, **kwargs)
+
+        for picking in self:
+            # Solo para Recepciones
+            if picking.picking_type_id.code != 'incoming':
+                continue
+
+            purchase = picking.purchase_id
+            if purchase and purchase.x_studio_comprado:
+                partner = purchase.partner_id
+                phone = partner.phone or partner.mobile
+                if not phone:
+                    continue
+
+                # Buscar plantilla y cuenta
+                template = self.env['whatsapp.template'].search([('name', '=', 'tecomprotumovil')], limit=1)
+                wa_account = self.env['whatsapp.account'].search([], limit=1)
+
+                if template and wa_account:
+                    # Crear mensaje de WhatsApp
+                    msg = self.env['whatsapp.message'].create({
+                        'mobile_number': phone,
+                        'wa_template_id': template.id,
+                        'wa_account_id': wa_account.id,
+                        'message_type': 'structured',
+                        'state': 'queued',
+                    })
+
+                    # Enviar mensaje automáticamente
+                    try:
+                        msg._send_message()
+                        picking.message_post(
+                            body="📲 Se ha enviado automáticamente el mensaje de WhatsApp “tecomprotumovil” al proveedor."
+                        )
+                    except Exception as e:
+                        picking.message_post(
+                            body=f"⚠️ Error al enviar mensaje de WhatsApp automático: {str(e)}"
+                        )
+
+        return res
