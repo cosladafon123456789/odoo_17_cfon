141ee836f9c37602601b41628e92d3424f6b603b
cosladafon123456789 <jc@cosladafon.com>
2025-10-21 10:11:08 +0200

    Add files via upload

---
+++ cf_tecomprotumovil_auto_whatsapp_v17_0_3_6_0/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "TeComproTuMovil – Envío automático WhatsApp",
+    "summary": "Envía la plantilla 'tecomprotumovil' al validar traslados (stock.picking). Evita duplicados por número y bloquea el flag en la compra.",
+    "version": "17.0.3.6.0",
+    "author": "CosladaFon",
+    "website": "https://www.cosladafon.com",
+    "category": "Stock/WhatsApp",
+    "license": "OEEL-1",
+    "depends": ["stock", "purchase_stock", "whatsapp"],
+    "data": [],
+    "application": false,
+    "installable": true
+}
