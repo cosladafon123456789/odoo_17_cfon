40aac731f44a595b38437a71575e0706eb60a9b8
cosladafon123456789 <jc@cosladafon.com>
2025-10-23 13:40:43 +0200

    Update __manifest__.py

--- cf_stock_bat100_final/__manifest__.py
+++ cf_stock_bat100_final/__manifest__.py
@@ -1,16 +1,10 @@
 {
-    'name': 'CF Stock BAT100',
-    'version': '17.0.2.0.0',
-    'summary': 'Añade campo BAT100 a números de serie y movimientos de stock',
-    'author': 'CosladaFon',
-    'website': 'https://www.cosladafon.com',
-    'category': 'Inventory',
+    'name': 'CF Stock BAT100 Persistente',
+    'version': '17.0.4.0.0',
     'depends': ['stock'],
     'data': [
-        'views/stock_lot_views.xml',
         'views/stock_move_line_views.xml',
     ],
-    'license': 'LGPL-3',
     'installable': True,
     'application': False,
 }
