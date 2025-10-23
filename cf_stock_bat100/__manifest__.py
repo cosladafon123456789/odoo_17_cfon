1c9a1e4fe6a0435f27f5e9f04181feb5e2877f0b
cosladafon123456789 <jc@cosladafon.com>
2025-10-23 11:16:12 +0200

    Add files via upload

--- cf_stock_bat100/__manifest__.py
+++ cf_stock_bat100/__manifest__.py
@@ -1,16 +1,16 @@
 {
-    "name": "CF Stock BAT100",
-    "version": "17.0.1.0.0",
-    "summary": "Casilla BAT100 junto a cada IMEI/serial en recepciones y entregas",
-    "author": "CosladaFon",
-    "website": "https://www.cosladafon.com",
-    "category": "Inventory",
-    "depends": ["stock"],
-    "data": [
-        "views/stock_lot_views.xml",
-        "views/stock_move_line_views.xml"
+    'name': 'CF Stock BAT100',
+    'version': '17.0.1.0.1',
+    'summary': 'Casilla BAT100 junto a cada IMEI/serial en recepciones y entregas',
+    'author': 'CosladaFon',
+    'website': 'https://www.cosladafon.com',
+    'category': 'Inventory',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_lot_views.xml',
+        'views/stock_move_line_views.xml'
     ],
-    "license": "LGPL-3",
-    "installable": true,
-    "application": false
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
 }
