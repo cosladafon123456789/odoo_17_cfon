ab155992c754cd698d181fbce98288698130b854
cosladafon123456789 <jc@cosladafon.com>
2025-10-23 13:40:00 +0200

    Update stock_move_line.py

--- cf_stock_bat100_final/models/stock_move_line.py
+++ cf_stock_bat100_final/models/stock_move_line.py
@@ -1,12 +1,37 @@
-from odoo import models, fields
+from odoo import models, fields, api
+
+class StockProductionLot(models.Model):
+    _inherit = 'stock.lot'
+
+    bat100 = fields.Boolean(string='BAT100', tracking=True)
+
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    bat100 = fields.Boolean(
-        string='BAT100',
-        related='lot_id.bat100',
-        readonly=False,
-        store=False,
-        help='Marca si el IMEI seleccionado tiene batería al 100% (guardado en el número de serie).'
-    )
+    bat100 = fields.Boolean(string='BAT100', tracking=True)
+
+    @api.onchange('lot_id')
+    def _onchange_lot_id(self):
+        """Carga el valor del lote si ya existe."""
+        for line in self:
+            if line.lot_id:
+                line.bat100 = line.lot_id.bat100
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Guarda el valor en el lote si existe."""
+        lines = super().create(vals_list)
+        for line in lines:
+            if line.lot_id:
+                line.lot_id.write({'bat100': line.bat100})
+        return lines
+
+    def write(self, vals):
+        """Actualiza también el lote al modificar la línea."""
+        res = super().write(vals)
+        if 'bat100' in vals:
+            for line in self:
+                if line.lot_id:
+                    line.lot_id.write({'bat100': vals['bat100']})
+        return res
