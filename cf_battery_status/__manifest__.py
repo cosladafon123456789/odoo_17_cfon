b3f1238404cd18ff69a6c79b1c4682f0ac9e39f0
cosladafon123456789 <jc@cosladafon.com>
2025-10-24 11:36:34 +0200

    Update __manifest__.py

--- cf_battery_status/__manifest__.py
+++ cf_battery_status/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     "name": "CF Battery Status",
-    "version": "17.0.1.3.0",
+    "version": "17.0.1.4.0",
     "summary": "Añade botón BAT100 en números de serie/lote",
     "author": "CosladaFon + ChatGPT",
     "license": "LGPL-3",
     "category": "Inventory/Inventory",
-    "depends": ["stock"],  # ✅ Solo stock, tu entorno ya incluye lotes dentro
+    "depends": ["stock"],
     "data": [
         "security/ir.model.access.csv",
-        "views/stock_production_lot_views.xml",
+        "views/stock_lot_views.xml",
     ],
     "installable": True,
     "application": False,
