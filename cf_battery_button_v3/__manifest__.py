a0b9f43f26bebddab6c73558b26a15a82d82a0f6
cosladafon123456789 <jc@cosladafon.com>
2025-10-24 11:57:51 +0200

    Add files via upload

---
+++ cf_battery_button_v3/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'CF Battery 100 Button v3',
+    'version': '3.0',
+    'depends': ['stock'],
+    'author': 'CosladaFon',
+    'category': 'Inventory',
+    'summary': 'Añade campo BAT100 en números de serie y recepciones con edición directa',
+    'data': [
+        'views/stock_lot_views.xml',
+        'views/stock_move_line_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
