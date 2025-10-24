981173ba4a8c522c67d45227dcd8d3a6f37bfa3a
cosladafon123456789 <jc@cosladafon.com>
2025-10-24 11:59:21 +0200

    Update stock_lot.py

--- cf_battery_button_v3/models/stock_lot.py
+++ cf_battery_button_v3/models/stock_lot.py
@@ -1,10 +1,13 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class StockLot(models.Model):
     _inherit = 'stock.lot'
 
     x_bat100 = fields.Boolean(string="Batería 100%", default=False)
 
+    @api.model
     def action_toggle_bat100(self):
+        """Alterna el estado de Batería 100%"""
         for record in self:
             record.x_bat100 = not record.x_bat100
+        return True
