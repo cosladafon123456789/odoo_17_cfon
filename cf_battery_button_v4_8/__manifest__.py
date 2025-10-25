3c8aa5f0d94bf285155d8465409075d64d716922
cosladafon123456789 <jc@cosladafon.com>
2025-10-25 11:08:59 +0200

    Update __manifest__.py

--- cf_battery_button_v4_8/__manifest__.py
+++ cf_battery_button_v4_8/__manifest__.py
@@ -1,13 +1,17 @@
 {
-    'name': 'CF Battery Button v4.8',
+    'name': 'CF Battery Button v4.9',
     'version': '1.0',
     'author': 'CosladaFon',
     'category': 'Inventory',
-    'summary': 'Botón Batería 100% en Lotes y Movimientos de Stock',
-    'description': "Agrega un campo de tipo toggle 'Batería 100%' que se puede marcar tanto desde el popup de movimiento de stock como desde la vista de lotes. El estado se guarda automáticamente en el lote vinculado.",
+    'summary': 'Botón Batería 100% en Lotes, Movimientos de Stock y Ubicaciones',
+    'description': """
+Este módulo agrega un campo tipo toggle 'Batería 100%' que se puede marcar tanto desde el popup
+de movimiento de stock como desde la vista de lotes. 
+El estado se guarda automáticamente en el lote vinculado y se muestra también en el panel de ubicaciones (vista Kanban).
+    """,
     'depends': ['stock'],
     'data': [
-        'views/stock_lot_views.xml',
+        'views/stock_lot_views.xml',       # contiene vistas de lotes y herencia de kanban de stock.quant
     ],
     'installable': True,
     'application': False,
