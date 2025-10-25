1c66cfd7746e0fcf628d0b4b97efb2ce6f164b12
cosladafon123456789 <jc@cosladafon.com>
2025-10-25 09:54:33 +0200

    Update __manifest__.py

--- cf_bateria_100/__manifest__.py
+++ cf_bateria_100/__manifest__.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
-{    'name': "CF Batería 100",
-    'summary': "Icono de batería 100% en kanban de números de serie y toggle en formulario",
-    'version': "17.0.1.0.0",
-    'author': "CosladaFon",
-    'website': "https://cosladafon.com",
-    'category': "Inventory/Inventory",
-    'depends': ["stock"],
-    'data': ["views/stock_lot_views.xml"],
-    'license': "LGPL-3",
-    'installable': true,
-    'application': false,
+{
+    'name': 'CF Batería 100',
+    'summary': 'Icono de batería 100% en kanban de números de serie y toggle en formulario',
+    'version': '17.0.1.0.0',
+    'author': 'CosladaFon',
+    'website': 'https://cosladafon.com',
+    'category': 'Inventory/Inventory',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_lot_views.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
 }
